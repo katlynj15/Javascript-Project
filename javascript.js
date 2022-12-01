598fa983dd6ff707e6e3b9d254ff5ec7c54013ba
katlynj15 <bookkateyj@gmail.com>
2022-12-01 01:42:22 +0000

    Message

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let who = ['My client','My customer','That lady','Her friend'];
+let action = ['damaged','cracked','split','broke'];
+let what = ['her nail', 'her finger', 'her toe'];
+let when = ['before the vacation','right after the appointment ','when I finished','satuday while partying','while she was exercising '];
--- src/app.js
+++ src/app.js
@@ -4,8 +4,41 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let who = ["My client", "My customer", "That lady", "Her friend"];
+let action = ["damaged", "cracked", "split", "broke"];
+let what = ["her nail", "her finger", "her toe"];
+let when = [
+  "before the vacation",
+  "right after the appointment ",
+  "when I finished",
+  "satuday while partying",
+  "while she was exercising "
+];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World!");
 };
+document.querySelector("#more").addEventListener("click", () => {
+  document.querySelector("#excuse").innerHTML = newExcuse();
+});
+
+function newExcuse() {
+  let who = ["My client", "My customer", "That lady", "Her friend"];
+  let action = ["damaged", "cracked", "split", "broke"];
+  let what = ["her nail", "her finger", "her toe"];
+  let when = [
+    "before the vacation",
+    "right after the appointment ",
+    "when I finished",
+    "satuday while partying",
+    "while she was exercising "
+  ];
+  let whoN = Math.floor(Math.random() * 4);
+  let actionN = Math.floor(Math.random() * 4);
+  let whatN = Math.floor(Math.random() * 3);
+  let whenN = Math.floor(Math.random() * 5);
+
+  return (
+    who[whoN] + " " + action[actionN] + " " + what[whatN] + " " + when[whenN]
+  );
+}
